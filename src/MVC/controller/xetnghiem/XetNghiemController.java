6dcc5b8e8959364b42c24d785c24ae4822825d5e
dungpt24082002 <dungpt24082002@gmail.com>
2023-06-27 17:02:44 +0700

    Update Controller

---
+++ src/MVC/controller/xetnghiem/XetNghiemController.java
@@ -0,0 +1,17 @@
+package MVC.controller.xetnghiem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class XetNghiemController {
+
+    @FXML
+    private TextField searchTextField;
+
+    @FXML
+    void search(MouseEvent event) {
+
+    }
+
+}
